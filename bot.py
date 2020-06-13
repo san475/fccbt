8625a7ed6646d4f7d2b24022f65d701607a7aaf5
Francisco Esteve <franj696@gmail.com>
2020-06-13 15:45:30 -0400

    added some checks and addgroup functionality, need to refactor into bot commands instead of calling everything using a bunch of fucking conditionals in an event handler ¯\_(ツ)_/¯

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import discord
 import asyncio
 from discord.ext.commands import Bot
 
+from time import sleep
 import signal
 import sys
 import ast
@@ -13,7 +14,7 @@ client = discord.Client()
 #ctxclient = Bot(description="My Cool Bot", command_prefix="!", pm_help = False, )
 
 def main():
-	client.run('NzIwODQxNzY5ODA1MjgzMzYw.XuL3cw.X2IEBRx-EIYjsNl7zZiEqJHvNZo')
+	client.run('NzIwODQxNzY5ODA1MjgzMzYw.XuUZoA.dhPiRyEGzPPVB3BdRModJuxjTsQ')
 
 #@ctxclient.command(pass_context=True)
 #async def whoami(ctx):
@@ -22,7 +23,7 @@ def main():
 		#await client.send_message(ctx.message.channel, msg)
 	#else:
 		#msg = "You're an average joe {0.author.mention}".format(ctx.message)  
-		#await client.send_message(ctx.message.channel, msg)
+		#wait client.send_message(ctx.message.channel, msg)
 
 @client.event
 async def on_ready():
@@ -39,36 +40,70 @@ async def on_message(message):
 		#await message.channel.send("/tts The king has spoken")
 	#await message.edit(content=message.content + " :jordanmoon:")
 
+	## ANOTHER ONE TO REFACTOR
 	if message.content.startswith('$hello'):
+		sleep(.5)
 		await message.channel.send('Hello')
 	if message.content.startswith('$jordanxplode'):
 		await message.channel.send('https://cdn.discordapp.com/attachments/678702504514551841/720849812068499456/video0.mov')
+	## ANOTHER ONE TO REFACTOR
 	if message.content.startswith('$groups'):
 		await message.channel.send('Printing group dictionary')
 		groupFile = open('groups.txt', 'r+')
 		string = groupFile.read()
 		if string:
 			groupDict = ast.literal_eval(string)
-			await message.channel.send(string)
-		await message.channel.send(string)
+			await message.channel.send(str(groupDict))
+		#await message.channel.send(string)
 		groupFile.close()
 
-	if message.content.startswith('$addmeto'):
-		group = message.content[8:]
-		print('You are adding yourself to the group: ' + group)
-		groupFile = open('groups.txt', 'r+')
+	## ANOTHER ONE TO REFACTOR
+	if message.content.startswith('$addgroup'):
+		group = message.content[9:].strip()
+		await message.channel.send('You are adding the group: ' + group)
+		if message.author.name not in ['san475', 'Chronite', 'Castronaut', 'Tylope', 'Mako']:
+			await message.channel.send('You\'re not in the white-list, bug off!')
+		else:
+			groupFile = open('groups.txt', 'r+')
+			string = groupFile.read()
+			groupFile.seek(0)
 
+			groupDict = {}
+
+			if(string):
+# convert: string -> dictionary
+				groupDict = ast.literal_eval(string)
+			if group in groupDict:
+				await message.channel.send('That group already exists...')
+			else:
+				groupDict[group] = []
+				await message.channel.send('Congrats, you just created the group: ' + group + '!')
+
+			groupFile.write(str(groupDict))
+
+			groupFile.truncate()
+			groupFile.close()
+
+	## ANOTHER ONE TO REFACTOR
+	if message.content.startswith('$addmeto'):
+		group = message.content[8:].strip()
+		await message.channel.send('You are adding yourself to the group: ' + group)
+
+		groupFile = open('groups.txt', 'r+')
 		string = groupFile.read()
 		groupFile.seek(0)
 
 		groupDict = {}
+
 		if(string):
-			# convert: string -> dictionary
+# convert: string -> dictionary
 			groupDict = ast.literal_eval(string)
-		if group in groupDict:
-			groupDict[group].append(message.author.name)
+		if group not in groupDict:
+			await message.channel.send('That group doesn\'t exist, contact an admin! <@260568153254395907>')
+		elif message.author.name in groupDict[group]:
+			await message.channel.send('You\'re already in the group ' + group + ' silly <:jordanmoon:720783841584742501>')
 		else:
-			await message.channel.send('That group doesn\'t exist, contact an admin!')
+			groupDict[group].append(message.author.name)
 
 		groupFile.write(str(groupDict))
 
@@ -79,7 +114,7 @@ async def on_message(message):
 	if message.content.startswith('$write'):
 		logFile = open('log.txt', 'r+')
 		logFile.write(message.content[6:])
-		logFile.write(str(message.author.name))
+		logFile.write('  :  ' + str(message.author.name))
 		logFile.close()
 
 
